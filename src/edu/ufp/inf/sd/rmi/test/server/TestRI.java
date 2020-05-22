cd7d4ce54b3d72152fbae4fe2e42a6dca868b520
David Capela <david.bc2.24@gmail.com>
2020-05-22 18:07:52 +0100

    add project structure (#1)
    * add project structure
    
    * update project structure
    
    * add rabbitmq core funtions

--- src/edu/ufp/inf/sd/rmi/test/server/TestRI.java
+++
@@ -1,9 +0,0 @@
-package edu.ufp.inf.sd.rmi.test.server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-
-public interface TestRI extends Remote {
-    String methodName() throws RemoteException;
-}
