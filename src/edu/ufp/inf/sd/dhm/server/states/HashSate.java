1a1a11e06ead7089af0c186eeca306f10557159d
Pedro <plsalmeida18@gmail.com>
2020-06-24 23:15:20 +0100

    progress status

--- src/edu/ufp/inf/sd/dhm/server/states/HashSate.java
+++ src/edu/ufp/inf/sd/dhm/server/states/HashSate.java
@@ -12,8 +12,8 @@ public class HashSate implements Serializable {
     int workerId;
     String ownerName;
     WorkerStatus status;
-    StringGroup original;           // StringGroup originally attached to this worker
-    StringGroup pending;            // StringGroup pending to mine ( not mined yet )
+    StringGroup original;    // StringGroup originally attached to this worker
+    StringGroup pending;     // StringGroup pending to mine ( not mined yet )
     String hash;      // String w/ the match
     String word;      // String w/ the word found
 
